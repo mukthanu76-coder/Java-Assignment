ff894464be6fac4183c0e730e11ea6f0f0e803d5
Muktha NU <Muktha NU@Muktha>
2026-04-21 21:21:51 +0530

    commit

---
+++ src/main/java/com/cookie/CookieServlet.java
@@ -0,0 +1,63 @@
+package com.cookie;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/CookieServlet")
+public class CookieServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("username");
+
+        int visitCount = 1;
+
+        // Get existing cookies
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("visitCount")) {
+                    visitCount = Integer.parseInt(c.getValue());
+                    visitCount++;
+                }
+            }
+        }
+
+        // Create cookies
+        Cookie nameCookie = new Cookie("username", name);
+        Cookie countCookie = new Cookie("visitCount", String.valueOf(visitCount));
+
+        // Set expiry (60 seconds)
+        nameCookie.setMaxAge(60);
+        countCookie.setMaxAge(60);
+
+        response.addCookie(nameCookie);
+        response.addCookie(countCookie);
+
+        // Output
+        out.println("<html><body>");
+        out.println("<h2>Welcome back " + name + "!</h2>");
+        out.println("<p>You have visited this page " + visitCount + " times.</p>");
+
+        out.println("<h3>List of Cookies:</h3>");
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                out.println("Name: " + c.getName() + 
+                            " | Value: " + c.getValue() + "<br>");
+            }
+        }
+
+        out.println("<br><a href='index.html'>Visit Again</a>");
+        out.println("</body></html>");
+    }
+}
